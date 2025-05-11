65808b40ba5d76d1f839f6aa1b3ed7c8a47db203
piyush <piyush.khadka2233@gmail.com>
2025-05-11 07:32:20 +0100

    created nav bar

---
+++ src/Home.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './styles/Home.css'
+function Home() {
+  
+  return (
+    <>
+    <nav>
+    <div className="nav">
+      <a>PKC</a>
+      <div className="bar"> 
+        <a>Home</a>
+        <a>About</a>
+        <a>Projects</a>
+      </div>
+    </div>
+    </nav>
+    <div>
+      <h1>PIYUSH KHADKA CHHETRI</h1>
+    </div>
+    </>
+  )
+}
+
+export default Home
