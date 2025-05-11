a0a11c610f701bda3513a25f3c33d7b307b91fa4
piyush <piyush.khadka2233@gmail.com>
2025-05-11 17:11:55 +0100

    used map to reuse code

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,9 +1,23 @@
-const Project =() => {
+import pneumoniaImg from '../assets/images/pneumonia.jpg'
 
+const projects=[{
+       title: 'Pneumonia Recognition using MONAI',
+       description: 'Pneumonia Recognition using MONAI is a deep learning project...',
+       image: pneumoniaImg,
+       id: 1,
+  }];
+
+const Project =() => {
   return (
-    <div >
+    <div>
       <h1 className="projects">Projects</h1>
-     
+        {projects.map((proj,id) => (
+          <div className="singleProject"  key={id}>
+            <img src={proj.image}/>
+            <h1>{proj.title}</h1>
+            <p>{proj.description}</p>
+          </div>
+        ))}
     </div>
 )}
 
