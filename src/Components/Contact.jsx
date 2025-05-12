52a835583ab913f9d8eca5103eba94a17ee3ca7d
piyush <piyush.khadka2233@gmail.com>
2025-05-12 15:38:12 +0100

    minor changes to contacts

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,17 +1,20 @@
 const Contact = () =>{
   return (
-    <form>
+    <form className="form">
+    <div>
       <label htmlFor="name">Name:</label>
       <input type="text" id="name" name="name" />
-
+    </div>
+    <div>
       <label htmlFor="email">Email:</label>
       <input type="email" id="email" name="email" />
-
+    </div>
+    <div>
       <label htmlFor="message">Message:</label>
       <textarea id="message" name="message"></textarea>
-
-      <button type="submit">Send Message</button>
-    </form>
+    </div>
+    <button type="submit">Send Message</button>
+  </form>
   )
 }
 
