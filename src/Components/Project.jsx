26105c1da0f23b0ec42853ae22f80ef77c10fe82
piyush <piyush.khadka2233@gmail.com>
2025-05-12 10:28:03 +0100

    made project card reusable

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -5,12 +5,16 @@ const projects=[{
        description: 'Pneumonia Recognition using MONAI is a deep learning project...',
        image: pneumoniaImg,
        id: 1,
-  }];
+  } , {title: 'Pneumonia Recognition using MONAI',
+       description: 'Pneumonia Recognition using MONAI is a deep learning project...',
+       image: pneumoniaImg,
+       id: 2,}];
 
 const Project =() => {
   return (
-    <div className="projects">
+    <div className='mainProject'>
       <h1>Projects</h1>
+      <div className="projects">
         {projects.map((proj,id) => (
           <div className="singleProject" key={id}>
             <img src={proj.image}/>
@@ -18,6 +22,7 @@ const Project =() => {
             <p>{proj.description}</p>
           </div>
         ))}
+      </div>
     </div>
 )}
 
