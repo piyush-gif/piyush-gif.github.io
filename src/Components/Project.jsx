28626ec0d36000c48d4d1ed1a5036b034d9be538
piyush <piyush.khadka2233@gmail.com>
2025-05-14 10:46:59 +0100

    added links to the github repo for each projects

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,29 +1,42 @@
 import pneumoniaImg from '../assets/images/pneumonia.jpg'
-
+import Dashboard from '../assets/images/power.png'
+import amazon from '../assets/images/amazon.png'
 const projects=[{
-       title: 'Pneumonia Recognition using MONAI',
-       description: 'Pneumonia Recognition using MONAI is a deep learning project...',
+       title: 'PNEUMONIA RECOGNITION',
        image: pneumoniaImg,
+       github:'https://github.com/piyush-gif/Pneumonia-Recognition-using-Monai-PRM-',
        id: 1,
-  } , {title: 'Pneumonia Recognition using MONAI',
-       description: 'Pneumonia Recognition using MONAI is a deep learning project...',
-       image: pneumoniaImg,
-       id: 2,}
-      , {title: 'Pneumonia Recognition using MONAI',
-       description: 'Pneumonia Recognition using MONAI is a deep learning project...',
-       image: pneumoniaImg,
-       id: 2,}];
+  },
+   {title: 'AMAZON CLONE',
+       image: amazon,
+       github: 'https://github.com/piyush-gif/AmazonJS',
+       id: 2,
+      }
+      , {title: 'DATA PROFFESIONAL SURVEY',
+       image: Dashboard,
+       github: 'https://github.com/piyush-gif/PorfolioProjects',
+       id: 2,
+      }];
 
 const Project =() => {
   return (
     <div className='mainProject'>
       <h1>Projects</h1>
+      <p>(more coming soon)</p>
       <div className="projects">
-        {projects.map((proj,id) => (
-          <div className="singleProject" key={id}>
-            <img src={proj.image}/>
-            <h2>{proj.title}</h2>
-            <p>{proj.description}</p>
+        {projects.map((proj) => (
+          <div className="singleProject" key={proj.id}>
+             <div className="imageContainer">
+              <img src={proj.image} alt={proj.title}/>
+              <a
+                href={proj.github}
+                className="projectTitle"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {proj.title}
+              </a>
+            </div>
           </div>
         ))}
       </div>
