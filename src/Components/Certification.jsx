52d6620f2f95acd3b7794d1b0b4f62490b968d24
piyush <piyush.khadka2233@gmail.com>
2025-05-14 13:17:30 +0100

    added css styling on certificates

--- src/Components/Certification.jsx
+++ src/Components/Certification.jsx
@@ -11,33 +11,33 @@ const Certification = () => {
   ]
   const [open, setOpen] = useState(null);
   return (
-    <>
-    <h1 className="header">Certifications</h1>
-    <div className="certifications"> 
-      <div className="certicard">
-        {content.map((cont) =>(
-          <div key={cont.id}>
-            <div className="certificates">
-              <h3>{cont.title}</h3>
-              <button  onClick={() =>setOpen(open === cont.id ? null : cont.id)}>drop</button>
-              </div>
-            {open === cont.id && (
-              <div className="drop-down-content">
-                <div>Skills</div>
-                    <ul>
-                      {cont.about.map((item, idx) => (
-                        <li key={idx}>{item}</li>
-                      ))}
-                    </ul>
-                      <a href={cont.link} target="_blank" rel="noreferrer" >CERTIFICATE</a>
+    <div className='cert'>
+      <h1>Certifications</h1>
+      <div className="certifications"> 
+        <div className="certicard">
+          {content.map((cont) =>(
+            <div key={cont.id}>
+              <div className="certificates">
+                <h3>{cont.title}</h3>
+                <button  onClick={() =>setOpen(open === cont.id ? null : cont.id)}>info</button>
                 </div>
-                  )}
+              {open === cont.id && (
+                <div className="drop-down-content">
+                  <div>Skills</div>
+                      <ul>
+                        {cont.about.map((item, idx) => (
+                          <li key={idx}>{item}</li>
+                        ))}
+                      </ul>
+                        <a href={cont.link} target="_blank" rel="noreferrer" >CERTIFICATE</a>
+                  </div>
+                    )}
 
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-    </>
 )};
 
 export default Certification;
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,20 +1,6 @@
 const Contact = () =>{
   return (
-    <form className="form">
-    <div>
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name" />
-    </div>
-    <div>
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" />
-    </div>
-    <div>
-      <label htmlFor="message">Message:</label>
-      <textarea id="message" name="message"></textarea>
-    </div>
-    <button type="submit">Send Message</button>
-  </form>
+    <img src="\assets\images\contactLogo.jpg"/>
   )
 }
 
